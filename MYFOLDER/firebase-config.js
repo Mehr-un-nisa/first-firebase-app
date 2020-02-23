24b6e0c78212ad1af6afaf236d8e30e661823311
Mehru <mehrunnisa533@gmail.com>
2020-02-23 17:42:53 +0500

    Initial commit

---
+++ MYFOLDER/firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAwv3bjK6XWcf5Mow3cnSAYjIbduz_iVos",
+    authDomain: "fir-project-c9ea3.firebaseapp.com",
+    databaseURL: "https://fir-project-c9ea3.firebaseio.com",
+    projectId: "fir-project-c9ea3",
+    storageBucket: "fir-project-c9ea3.appspot.com",
+    messagingSenderId: "672144616725",
+    appId: "1:672144616725:web:d3540781a1c9188dc10d1c"
+  };
